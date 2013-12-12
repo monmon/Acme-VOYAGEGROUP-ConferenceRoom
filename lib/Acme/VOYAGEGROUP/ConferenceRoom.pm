a177703ea2f9f8cab1e8a0385678395258e8dd44
monmon <lesamoureuses@gmail.com>
2013-12-12 22:06:58 +0900

    initial import

---
+++ lib/Acme/VOYAGEGROUP/ConferenceRoom.pm
@@ -0,0 +1,39 @@
+package Acme::VOYAGEGROUP::ConferenceRoom;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::VOYAGEGROUP::ConferenceRoom - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::VOYAGEGROUP::ConferenceRoom;
+
+=head1 DESCRIPTION
+
+Acme::VOYAGEGROUP::ConferenceRoom is ...
+
+=head1 LICENSE
+
+Copyright (C) monmon.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+monmon E<lt>lesamoureuses@gmail.comE<gt>
+
+=cut
+
